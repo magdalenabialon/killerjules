fd81d85a0ea194b566e6dfa342fb14f78754aa28
Magdalena Bialon <magdalenabialon@gmail.com>
2016-09-23 15:53:16 +1000

    slider - very basic :)

--- src/LandingSlider.js
+++ src/LandingSlider.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './LandingSlider.css';
 
 import ImageGallery from 'react-image-gallery';
-
+// import ImageGallery from '../src/ImageGallery';
 
 
 class LandingSlider extends Component {
@@ -10,30 +10,50 @@ class LandingSlider extends Component {
     handleImageLoad(event) {
       console.log('Image loaded ', event.target)
     }
+
+      constructor() {
+        super();
+        this.state = {
+          showIndex: false,
+          slideOnThumbnailHover: false,
+          showBullets: true,
+          infinite: true,
+          showThumbnails: true,
+          showFullscreenButton: true,
+          showGalleryFullscreenButton: true,
+          showPlayButton: false,
+          showGalleryPlayButton: false,
+          showNav: true,
+          slideInterval: 2000,
+          showVideo: {},
+          autoPlay: true,
+        };
+      }
+
   
   render() {
 
 
     const images = [
       {
-        original: 'http://lorempixel.com/1000/600/nature/1/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/1/',
-        originalClass: 'featured-slide',
-        thumbnailClass: 'featured-thumb',
-        originalAlt: 'original-alt',
-        thumbnailAlt: 'thumbnail-alt',
-        thumbnailLabel: 'Optional',
-        description: 'Killer Jules',
-        srcSet: 'Optional srcset (responsive images src)',
-        sizes: 'Optional sizes (image sizes relative to the breakpoint)'
+        original: 'https://static.wixstatic.com/media/19c007_6bd37bf9b5a1446fb1568ff3a3c05489.jpg/v1/fill/w_207,h_279,al_c,q_80,usm_0.66_1.00_0.01/19c007_6bd37bf9b5a1446fb1568ff3a3c05489.jpg',
+        // thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+        // originalClass: 'featured-slide',
+        // thumbnailClass: 'featured-thumb',
+        // originalAlt: 'original-alt',
+        // thumbnailAlt: 'thumbnail-alt',
+        // thumbnailLabel: 'Optional',
+        // description: 'Killer Jules',
+        // srcSet: 'Optional srcset (responsive images src)',
+        // sizes: 'Optional sizes (image sizes relative to the breakpoint)',
       },
       {
-        original: 'http://lorempixel.com/1000/600/nature/2/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+        original: 'https://static.wixstatic.com/media/19c007_9ed1a9637c71448da975d26d63830ac2.jpg/v1/fill/w_207,h_213,al_c,q_80,usm_0.66_1.00_0.01/19c007_9ed1a9637c71448da975d26d63830ac2.jpg',
+        // thumbnail: 'http://lorempixel.com/250/150/nature/2/'
       },
       {
-        original: 'http://lorempixel.com/1000/600/nature/3/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+        original: 'https://static.wixstatic.com/media/19c007_25d11e555a1842aba4bf775b707bf18c.jpg/v1/fill/w_380,h_507,al_c,q_90,usm_0.66_1.00_0.01/19c007_25d11e555a1842aba4bf775b707bf18c.jpg',
+        // thumbnail: 'http://lorempixel.com/250/150/nature/3/'
       }
     ]
 
@@ -43,12 +63,12 @@ class LandingSlider extends Component {
       
       <div className="LandingSliderWrapper">
           
-          <h1>here will go SLIDER</h1>
                 <ImageGallery
                   ref={i => this._imageGallery = i}
                   items={images}
-                  slideInterval={1000}
+                  slideInterval={2500}
                   onImageLoad={this.handleImageLoad}/>
+
 
 
       </div>
