b9dfd06e187fc982398e855a38e0ca5e9ce7f86f
Magdalena Bialon <magdalenabialon@gmail.com>
2016-09-24 20:55:06 +1000

    slider img from net - to do - local ones

--- src/LandingSlider.js
+++ src/LandingSlider.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './LandingSlider.css';
 
 import ImageGallery from 'react-image-gallery';
-// import ImageGallery from '../src/ImageGallery';
+// import ImageGallery from './src';
 
 
 class LandingSlider extends Component {
@@ -18,8 +18,8 @@ class LandingSlider extends Component {
 
     const images = [
       {
-        original: 'https://static.wixstatic.com/media/19c007_6bd37bf9b5a1446fb1568ff3a3c05489.jpg/v1/fill/w_207,h_279,al_c,q_80,usm_0.66_1.00_0.01/19c007_6bd37bf9b5a1446fb1568ff3a3c05489.jpg',
-        // thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+        // original: 'kj1.jpg'
+        original: 'https://static.wixstatic.com/media/19c007_782634c7e1a544a3b806dde1c74787be.jpg/v1/fill/w_551,h_551,al_c,q_90,usm_0.66_1.00_0.01/19c007_782634c7e1a544a3b806dde1c74787be.jpg'
         // originalClass: 'featured-slide',
         // thumbnailClass: 'featured-thumb',
         // originalAlt: 'original-alt',
@@ -27,15 +27,20 @@ class LandingSlider extends Component {
         // thumbnailLabel: 'Optional',
         // description: 'Killer Jules',
         // srcSet: 'Optional srcset (responsive images src)',
-        // sizes: 'Optional sizes (image sizes relative to the breakpoint)',
+        // sizes:
       },
       {
-        original: 'https://static.wixstatic.com/media/19c007_9ed1a9637c71448da975d26d63830ac2.jpg/v1/fill/w_207,h_213,al_c,q_80,usm_0.66_1.00_0.01/19c007_9ed1a9637c71448da975d26d63830ac2.jpg',
-        // thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+        // original: './kj2.jpg'
+        original: 'https://static.wixstatic.com/media/19c007_977764e4e3f647c1876a9a8d2a76fa44.jpg/v1/fill/w_412,h_551,al_c,q_90,usm_0.66_1.00_0.01/19c007_977764e4e3f647c1876a9a8d2a76fa44.jpg'
       },
       {
-        original: 'https://static.wixstatic.com/media/19c007_25d11e555a1842aba4bf775b707bf18c.jpg/v1/fill/w_380,h_507,al_c,q_90,usm_0.66_1.00_0.01/19c007_25d11e555a1842aba4bf775b707bf18c.jpg',
-        // thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+        // original: './kj3.jpg'
+        original: 'https://static.wixstatic.com/media/19c007_25d11e555a1842aba4bf775b707bf18c.jpg/v1/fill/w_380,h_507,al_c,q_90,usm_0.66_1.00_0.01/19c007_25d11e555a1842aba4bf775b707bf18c.jpg'
+      },
+      {
+        original: 'https://static.wixstatic.com/media/19c007_cfbd3c11be954e26a73f504bcbf813f6.jpg/v1/fill/w_412,h_551,al_c,q_90,usm_0.66_1.00_0.01/19c007_cfbd3c11be954e26a73f504bcbf813f6.jpg'
+        // original: './kj4.jpg', renderItem: './kj4.jpg'
+        // original: './kj4.jpg'
       }
     ]
 
@@ -62,6 +67,34 @@ class LandingSlider extends Component {
       </div>
     
     );
+
+    //   renderItem(item) {
+    //     const onImageError = this.props.onImageError || this._handleImageError
+
+    //     return (
+
+
+
+          
+    //       <div className='LandingSliderWrapper'>
+    //         <img
+    //             src={item.original}
+    //             // alt={item.originalAlt}
+    //             // srcSet={item.srcSet}
+    //             // sizes={item.sizes}
+    //             onLoad={this.props.onImageLoad}
+    //             onError={onImageError.bind(this)}
+    //         />
+    //         {
+    //           item.description &&
+    //             <span className='image-gallery-description'>
+    //               {item.description}
+    //             </span>
+    //         }
+    //       </div>
+    //     )
+    // }
+
   }
 }
 
