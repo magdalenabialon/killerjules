297167b09a6d37c2612eea369635419708f3d66e
Magdalena Bialon <magdalenabialon@gmail.com>
2016-09-27 09:32:52 +1000

    mapbox works to do add marker with location

--- src/GroupKillerMap.js
+++ src/GroupKillerMap.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import './GroupKillerPage.css';
+// import MapGL from 'react-map-gl';
+import ReactMapboxGl, { Layer, Feature, ZoomControl } from "react-mapbox-gl";
+// import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+
+const { accessToken } = {
+                      "accessToken": "pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A",
+                      "style": "mapbox://styles/mapbox/streets-v8"
+                    }
 
 
 
-class GroupKillerGallery extends Component {
-  render() {
-    return (
+class GroupKillerMap extends Component {
 
-          <img className = "map" src={require('./ImageGallery/map_prahran.jpg')} />
+    state = {
+      popup: null,
+      center: [ 144.995162, -37.849719 ]
+    };
+
+
+      render() {
+        return (
+
+          <div id="map">
+
+            <ReactMapboxGl id="map"
+              style="mapbox://styles/mapbox/outdoors-v9"
+              accessToken={accessToken}
+              center={this.state.center}
+              movingMethod="jumpTo"
+              containerStyle={{ height: "100%", width: "100%" }}>
+            </ReactMapboxGl>
+
+          </div>
+
+
+
+
     );
   }
+
 }
 
-export default GroupKillerGallery;
+export default GroupKillerMap;
 
 
-
+          // <MapGL id="map" width={400} height={400} latitude={-37.849719} longitude={144.995162}
+          //   zoom={8} onChangeViewport={(viewport) => {
+          //     const {latitude, longitude, zoom} = viewport;
+          //   }}
+          // />
